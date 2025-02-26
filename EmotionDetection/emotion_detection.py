0bbe6aa0fe7a6ced70f26234ebf2391e040a344a
xs-abzal <xs_abzal@yahoo.com>
2025-02-25 21:08:39 -0500

    task4

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,6 @@
 import requests, json
 
-def emotion_detection(text_to_analyze):
+def emotion_detector(text_to_analyze):
     '''
     input  ---> text_to_analyze
     return ---> reponse of 'emotion detection' using Watson NLP. return output variable as a dictionary.
@@ -39,3 +39,29 @@ def emotion_detection(text_to_analyze):
     output['dominant_emotion'] = max(output, key=output.get)
     
     return output
+
+
+
+
+
+
+
+
+
+# import requests
+
+# def emotion_detector(text_to_analyze):
+#     '''
+#     input  ---> text_to_analyze
+#     return ---> reponse of 'emotion detection' using Watson NLP.
+#     '''
+
+#     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+#     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+#     input_json =  {"raw_document": {"text": text_to_analyze}}
+
+#     ### send post request 
+#     response = requests.post(url=url, headers=headers, json=input_json)
+    
+#     ### return reponse as a text   
+#     return response.text
